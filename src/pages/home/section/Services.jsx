86f9624604952b3155fbad344ba289cd19b81cc5
roniekun <roniebenitez01@gmail.com>
2024-04-28 13:17:07 +0800

    up

--- src/pages/home/section/Services.jsx
+++ src/pages/home/section/Services.jsx
@@ -4,7 +4,7 @@ const Services = () => {
   return (
           <div className='p-[5vw] flex flex-col lg:gap-5 gap-2 mt-14'>
               <h1 className='uppercase text-2xl font-semibold primary-font my-5 text-blue-600'>what i do</h1>
-              <p className='text-xl leading-tight md:w-3/4  self-end primary-font'>
+              <p className='text-xl leading-tight md:w-3/4  self-end primary-font indent-10'>
                 My goal is to provide a digital experience that serves a meaningful purpose while also giving your business a significant advantage.</p> 
           </div>
   )
