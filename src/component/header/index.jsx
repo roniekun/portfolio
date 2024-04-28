9cec1f3ae6f354e0060cf39ca51a99943572cfe1
roniekun <roniebenitez01@gmail.com>
2024-04-28 10:42:39 +0800

    update

--- src/component/header/index.jsx
+++ src/component/header/index.jsx
@@ -19,39 +19,43 @@ const Header = () => {
   const header = useRef(null)
   const nav = useRef(null)
 
-  const handleClose = () => {
-    setToggleMenu(false)
-  }
+  useEffect(() => {
+    if(isToggleMenu){
+      gsap.to(nav.current,{height: 'auto',  ease:'power2.inOut'})
+      gsap.fromTo(nav.current,{duration: .5, delay: 1, y: -5, ease: 'power1.in'},{delay: .3, y: 0, opacity: 1})
+    }
+    else{
+      gsap.to(nav.current,{
+          height: 0,
+          opacity: 0
+        })
+    }
+  }, [isToggleMenu, isMobile])
 
-useEffect(() => {
-  if(isToggleMenu){
-    gsap.to(nav.current,{height: 'auto',  ease:'power2.inOut'})
-    gsap.fromTo(nav.current,{duration: .5, delay: 1, y: -5, ease: 'power1.in'},{delay: .3, y: 0, opacity: 1})
-  }
-
-  else{
-    gsap.to(nav.current,{
-        height: 0,
-        opacity: 0
-      })
-  }
-
-}, [isToggleMenu, isMobile])
+  useGSAP(()=>{
+    if(isScrolled){
+    gsap.to(header.current,{opacity: 0, duration: .3})
+    }
+    else{
+      gsap.to(header.current,{opacity: 1,duration: .3})
+    }
+  },{dependencies:[isScrolled] })
 
   return (
     <motion.header
-     initial={{opacity: 0}}
-     animate={{opacity: 1, transition:{ duration: .3, delay: .3}}}
       ref={header}
-      className={`mt-5 fixed h-auto z-20  overflow-hidden w-auto bg-opacity-[10%] bg-white backdrop-blur-lg bg-blend-difference  rounded-3xl transform left-1/2 -translate-x-1/2 opacity-0 `}>
+      className={`md:mt-5 mt-3 fixed h-auto z-20  overflow-hidden w-auto bg-opacity-[10%] bg-white backdrop-blur-lg bg-blend-difference  rounded-3xl transform left-1/2 -translate-x-1/2`}>
           <section className='flex flex-col  md:w-[calc(100vw-100px)] w-[calc(100vw-50px)  overflow-hidden'>
-        <div
-            className={`flex relative  justify-between  items-center  md:h-16 h-[3.5rem]  z-0  w-[90vw]`}>
+        <motion.div
+          nitial={{opacity:0}}
+          animate={{opacity:1}}
+          transition={{delay:.3, durationg:.3}}
+            className={`flex relative  justify-between  items-center  md:h-16 h-[3.5rem]  z-0  w-[90vw] opacity-0`}>
             <div >
               <Logo />
             </div>
               {isMobile ? <Menu /> : <Nav />}
-        </div>
+        </motion.div>
           </section>
 
         <section
--- src/pages/home/section/Services.jsx
+++ src/pages/home/section/Services.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Services = () => {
   return (
-          <div className='p-[5vw] flex flex-col gap-5 mt-20'>
-              <h1 className='uppercase text-3xl font-semibold primary-font my-5'>what i do</h1>
-              <p className='text-xl leading-tight md:w-3/4  self-end'>
+          <div className='p-[5vw] flex flex-col lg:gap-5 gap-2 mt-14'>
+              <h1 className='uppercase text-2xl font-semibold primary-font my-5 text-blue-600'>what i do</h1>
+              <p className='text-xl leading-tight md:w-3/4  self-end primary-font'>
                 My goal is to provide a digital experience that serves a meaningful purpose while also giving your business a significant advantage.</p> 
           </div>
   )
