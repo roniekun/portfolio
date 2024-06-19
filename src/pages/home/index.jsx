038263a1df47fd86761e1bcee3c150c606c3949b
roniekun <roniebenitez01@gmail.com>
2024-06-19 11:34:47 +0800

    up

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -127,7 +127,7 @@ const Home = () => {
       <section
         ref={home}
         className="flex flex-col relative  text-neutral-300 lg:gap-y-5 gap-y-5 lg:h-[800px] min-h-[90vh]
-         z-0 overflow-hidden bg-stone-950 justify-center items-end"
+         z-0  bg-stone-950 justify-center items-center"
       >
         <div
           ref={hero}
