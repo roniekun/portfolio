ee6576322d2d5876fc2ecadb8abb53341ccbb77e
roniekun <roniebenitez01@gmail.com>
2024-07-10 17:04:24 +0800

    up

--- src/component/footer/index.jsx
+++ src/component/footer/index.jsx
@@ -15,13 +15,13 @@ const Footer = forwardRef((props, ref) => {
   return (
     <main
       ref={ref}
-      className="relative h-screen"
+      className="relative h-screen bg-stone-950"
       // style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}
     >
-      <div className="relative h-[calc(100vh+800px)] -top-[100vh]">
-        <div className="sticky top-[calc(100vh-100vh)]">
-          <footer className="relative  min-h-[800px] w-full  h-auto flex pt-[100px] p-[5vw] flex-col lg:p-[10vw] md:py-20 font-primary bg-stone-950 gap-10">
-            <section className="relative flex-col flex w-full h-full flex-wrap text-blue-50 justify-center items-center gap-7">
+      <div className="relative h-[calc(100vh+100vh)] -top-[100vh]">
+        <div className="sticky top-[calc(100vh-100vh)] ">
+          <footer className="relative h-full l w-full justify-start  flex items-start pt-[100px] p-[5vw] flex-col lg:p-[10vw] md:py-20 font-primary  gap-10 overflow-scroll">
+            <section className="relative flex-1 flex-col flex w-full h-full flex-wrap text-blue-50 justify-center items-center gap-7">
               <h3 className="font-medium text-base md:text-lg text-lime-400 imelg:leading-10 md:leading-9 leading-8 mt-[5vw]">
                 Thinking of a project?
               </h3>
